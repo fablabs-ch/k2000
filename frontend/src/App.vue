7e6cd30a7429bcf21d506a4227330618e7b8edd7
Fabien Dubosson <fabien.dubosson@gmail.com>
2017-10-21 15:09:37 +0200

    Fix frontend layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,13 @@
 <template>
-  <v-app>
-    <v-navigation-drawer app>
+
+  <v-app id="inspire" dark>
+    <v-navigation-drawer
+      clipped
+      persistent
+      v-model="drawer"
+      enable-resize-watcher
+      app
+    >
       <v-list dense>
         <v-list-tile @click="">
           <v-list-tile-action>
@@ -20,24 +27,31 @@
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar app>
+    <v-toolbar app fixed clipped-left>
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title>Application</v-toolbar-title>
     </v-toolbar>
     <main>
       <v-content>
-        <v-container fluid>
-          <router-view></router-view>
+        <v-container fluid fill-height>
+          <v-layout>
+            <router-view></router-view>
+          </v-layout>
         </v-container>
       </v-content>
     </main>
-    <v-footer app></v-footer>
+    <v-footer app fixed>
+      <span>&copy; 2017</span>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data: () => ({
+    drawer: true
+  })
 }
 </script>
 
