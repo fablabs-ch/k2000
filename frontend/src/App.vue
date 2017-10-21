52233df8d5107624257d880f1848cd0c4706d054
Fabien Dubosson <fabien.dubosson@gmail.com>
2017-10-21 16:03:47 +0200

    Add Settings page and likns in menu

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,22 +9,22 @@
       app
     >
       <v-list dense>
-        <v-list-tile @click="">
+        <router-link :to="{name: 'home'}" tag="v-list-tile" active-class="active" exact>
           <v-list-tile-action>
             <v-icon>dashboard</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
             <v-list-tile-title>Dashboard</v-list-tile-title>
           </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile @click="">
+        </router-link>
+        <router-link :to="{name: 'settings'}" tag="v-list-tile" active-class="active" exact>
           <v-list-tile-action>
             <v-icon>settings</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
             <v-list-tile-title>Settings</v-list-tile-title>
           </v-list-tile-content>
-        </v-list-tile>
+        </router-link>
       </v-list>
     </v-navigation-drawer>
     <v-toolbar app fixed clipped-left>
