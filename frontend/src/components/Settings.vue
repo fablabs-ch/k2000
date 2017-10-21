864624846f4ff8a60a891882ddaa546341e49c67
Fabien Dubosson <fabien.dubosson@gmail.com>
2017-10-21 16:40:20 +0200

    Add notification toast

--- frontend/src/components/Settings.vue
+++ frontend/src/components/Settings.vue
@@ -1,17 +1,36 @@
 <template>
-  <v-btn color="primary" @click="tare()">Primary</v-btn>
+  <div>
+    <v-btn color="primary" @click="tare()">Primary</v-btn>
+    <v-snackbar timeout=2000
+                top=true
+                vertical=true
+                v-model="snackbar">
+      {{ snacktext }}
+      <v-btn flat color="pink" @click.native="snackbar = false">Close</v-btn>
+    </v-snackbar>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'settings',
+  data: function () {
+    return {
+      snackbar: false,
+      snacktext: ''
+    }
+  },
   methods: {
     tare: function () {
       this.$http.get('/').then(response => {
-        console.log('Ça marcheeee.')
+        this.notify('Ça marcheeee.')
       }, response => {
-        console.log('Ça marche paaaaas.')
+        this.notify('Ça ne marche pas.')
       })
+    },
+    notify: function (message) {
+      this.snacktext = message
+      this.snackbar = true
     }
   }
 }
