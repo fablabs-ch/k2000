1f2bb357f50f68671928a718d3a7f1aa8b3b49c0
Boris Fritscher <boris@fritscher.ch>
2017-10-22 17:06:22 +0200

    Ingredient list page with ingredientListItems for each ingridient and an Ingredienttype selector

---
+++ frontend/src/components/IngredientListItem.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-list-tile>
+    <v-list-tile-action>
+      <v-select
+        label="Type"
+        :items="['ALCOOL', 'MINERAL', 'SIRUP', 'OTHER']"
+        v-model="value.type"
+        max-height="auto"
+      >
+        <template slot="selection" slot-scope="data">
+          <ingredient-type :type="data.item"></ingredient-type>
+        </template>
+        <template slot="item" slot-scope="data">
+          <ingredient-type :type="data.item" label></ingredient-type>
+        </template>
+    </v-select>
+    </v-list-tile-action>
+    <v-list-tile-content>
+      <v-text-field name="name" label="Name" v-model="value.name"></v-text-field>
+    </v-list-tile-content>
+    <v-list-tile-action>
+      <color-picker v-model="value.color"></color-picker>
+    </v-list-tile-action>
+  </v-list-tile>
+</template>
+
+<script>
+import IngredientType from '@/components/IngredientType'
+import ColorPicker from '@/components/ColorPicker'
+
+export default {
+  props: ['value'],
+  data () {
+    return {
+      e11: 'MINERAL',
+      color: '#ff0000'
+    }
+  },
+  methods: {
+    save () {
+      // TODO save / new ?
+    }
+  },
+  components: {
+    IngredientType,
+    ColorPicker
+  }
+}
+</script>
+
+<style>
+
+</style>
--- frontend/src/components/Ingredients.vue
+++ frontend/src/components/Ingredients.vue
@@ -1,12 +1,25 @@
 <template>
-  <div>
-      ingredients
-  </div>
+  <v-container fluid>
+    <v-layout column justify-space-around>
+      <h3><img src="../assets/picnic-basket-full-of-raw-vegetables.svg" alt="ingredients"> Ingredients</h3>
+      <v-list two-line v-for="(ingrident, id) in $root.ingredients" :key="ingrident.id">
+        <ingredient-list-item :value="ingrident"></ingredient-list-item>
+      </v-list>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-export default {
+import IngredientListItem from '@/components/IngredientListItem'
 
+export default {
+  data () {
+    return {
+    }
+  },
+  components: {
+    IngredientListItem
+  }
 }
 </script>
 
